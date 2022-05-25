b8f589a5f8082d5b303b909834b50178804e238d
Mony Machado <monymachad@gmail.com>
2022-05-25 01:07:11 -0300

    [camelsample-canel-prcpio] patterns

--- samplecamel/src/main/java/com/ladyjava/camel/samplecamel/RecipientListBean.java
+++ samplecamel/src/main/java/com/ladyjava/camel/samplecamel/RecipientListBean.java
@@ -1,5 +1,22 @@
 package com.ladyjava.camel.samplecamel;
 
+import org.apache.camel.RecipientList;
+import org.apache.camel.language.xpath.XPath;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
 public class RecipientListBean {
+	@RecipientList
+	public String[] route(@XPath("/order/@customer") String customer) {
+		if (isGoldCustomer(customer)) {
+			return new String[] {"jms:accounting", "jms:production"};
+		} else {
+			return new String[] {"jms:accounting"};
+		}
+	}
+	
+	private void isGoldCustomer(String customer) {
+		return customer.equals("honda");
+	}
 
 }
--- samplecamel/src/main/java/com/ladyjava/camel/samplecamel/RouteBuilderWithRecipientListBean.java
+++ samplecamel/src/main/java/com/ladyjava/camel/samplecamel/RouteBuilderWithRecipientListBean.java
@@ -1,28 +1,67 @@
 package com.ladyjava.camel.samplecamel;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.tooling.model.Strings;
 
 /**
  * A Camel Java DSL Router
  */
-public class RouteBuilderWithRecipientListBean extends RouteBuilder {
+public class RouteBuilderWithRecipientListBean {
 
-    /**
-     * Let's configure the Camel routing rules using Java code...
-     */
-    public void configure() {
+	public static void main(Strings args[]) throws Exception {
+		// create CamelContext
+		CamelContext context = new DefaultCamelContext();
 
-        // here is a sample which processes the input files
-        // (leaving them in place - see the 'noop' flag)
-        // then performs content based routing on the message using XPath
-        from("file:src/data?noop=true")
-            .choice()
-                .when(xpath("/person/city = 'London'"))
-                    .log("UK message")
-                    .to("file:target/messages/uk")
-                .otherwise()
-                    .log("Other message")
-                    .to("file:target/messages/others");
-    }
+		// connect to embeldded ActiveMQ JMS broker
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+		context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
+		// add our route to the CamelContext
+		context.addRoutes(new RouteBuilder() {
+			@Overide
+			public void configure() {
+        // load file orders from src/data into the JMS queue				
+		from("file:src/data?noop=true").to("jms:incomingOrders");
+		
+        // content-based router
+				from("jms:incomingOrders\")
+					.choice()
+						.when(header("CamelFileName").endsWith(".xml"))
+							.to("jms:xmlOrders\")"
+						.when(header("CamelFileName").regex("^.*(csv|csl)$\")"
+							.to(\"jms:csvOrders\")
+						.otherwise() 
+							.to(\"jms:badOrders\"); 
+			
+				from("jms:xmlOrders").bean(RecipientListBean.class);
+
+				// test that our route is working
+				from("jms:accounting").process(new Processor()) {				
+                public void process(Exchange exchange) throws Exception {
+                	System.out.println("Accounting received order: "
+                			+ exchange.getIn().getHeader("CamelFileName"));
+                 }
+                });
+				from("jms:production").process(new Processor()) {				
+				public void process(Exchange exchange) throws Exception {
+					System.out.println("Productionreceived order: "
+							+ exchange.getIn().getHeader("CamelFileName"));
+		     }
+        });				
+	  }
+     });
+    // start the route an dlet it do its work
+	context.start();
+	Thread.sleep(2000);
+	
+	// stop the CamelContext
+	context.stop();
+	
+	}
 }
