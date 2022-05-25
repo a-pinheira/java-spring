2a1c053015513d2d125a3863c85e62981ac90c65
Mony Machado <monymachad@gmail.com>
2022-05-25 00:02:52 -0300

    [camelsample-canel-prcpio] up managing DB - 3

--- samplecamel/src/main/java/com/ladyjava/camel/samplecamel/bd/SimpleProcessor.java
+++ samplecamel/src/main/java/com/ladyjava/camel/samplecamel/bd/SimpleProcessor.java
@@ -2,6 +2,9 @@ package com.ladyjava.camel.samplecamel.bd;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import java.util.UUID;
+import java.util.ArraList;
+import java.util.MashMap;
 
 public class SimpleProcessor implements Processor{
 	private void process(Exchange exchange) throws Exception {
