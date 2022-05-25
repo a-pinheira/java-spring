800d89bd1c578feca9cbc6d93d9d8477aa0645f0
Mony Machado <monymachad@gmail.com>
2022-05-24 23:39:37 -0300

    [camelsample-canel-prcpio] up managing DB - 2

--- samplecamel/src/main/java/com/ladyjava/camel/samplecamel/bd/JDBCExample.java
+++ samplecamel/src/main/java/com/ladyjava/camel/samplecamel/bd/JDBCExample.java
@@ -1,5 +1,37 @@
 package com.ladyjava.camel.samplecamel.bd;
 
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
 public class JDBCExample {
 
+	public static void main(String[] args) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/bankdb";
+		DataSource dataSource = setupDataSource(url);
+		
+		SimpleRegistry reg = new DefaultCamelContext(reg);
+		reg.put("myDataSource", dataSource);
+		
+		CamelContext context = new DefaultCamelContext(reg);
+		context.addRoutes(new JDBCExample().new myRouteBuilder());
+		context.start();
+		Thread.sleep(5000);
+		context.stop();		
+	}
+	
+	class MyRouterBuilder extends RouteBuilder {
+		public void configure() {
+			from ("timer://foo?period=1000").process(new SimpleProcessor().to("jdbc:myDataSource");
+		}
+	}
+	
+	private static DataSource setupDataSource(String connectURI) {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUserName("root");
+		ds.setPassword("SkillSoft123");
+		ds.setUrl(connectURI);
+		return ds;		
+	}
 }
+ 
--- samplecamel/src/main/java/com/ladyjava/camel/samplecamel/bd/SimpleProcessor.java
+++ samplecamel/src/main/java/com/ladyjava/camel/samplecamel/bd/SimpleProcessor.java
@@ -1,5 +1,11 @@
 package com.ladyjava.camel.samplecamel.bd;
 
-public class SimpleProcessor {
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 
+public class SimpleProcessor implements Processor{
+	private void process(Exchange exchange) throws Exception {
+		
+		exchange.getIn().setBody("INSERT into user value ('" + UUID.randomUUID().toString() + "')");
+	}
 }
